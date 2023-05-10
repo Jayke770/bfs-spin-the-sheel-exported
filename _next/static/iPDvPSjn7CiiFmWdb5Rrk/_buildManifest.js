a3d4073187066eec21de452d6955b1dec66a9b83
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2023-05-10 12:40:40 +0800

    .

---
+++ _next/static/iPDvPSjn7CiiFmWdb5Rrk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/408-24847ac02f18d7ee.js","static/chunks/pages/index-fb544130e84a3295.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
